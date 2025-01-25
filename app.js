7be4fb786caae55b734a11ac123d1afd4c0088a9
mlarsen <174657206+mlarsen-source@users.noreply.github.com>
2025-01-25 01:40:58 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+const app = express();
+
+
+app.use(express.static('public'));
+
+
+app.get('/',(req, res)=>{
+
+  res.sendFile(`${import.meta.dirname}/views/home.html`);
+
+});
+
+
+app.listen(3000, ()=>{
+
+  console.log("Server running at http://localhost:3000")
+
+});
